be7605aeb5f4d19040d53747f757b2f39ef3eb57
skoc <skoc10ds@gmail.com>
2021-10-13 21:42:39 +0300

    flask_4

---
+++ python/hands-on/flask-04-handling-forms-POST-GET-Methods/flask-04-handling-forms/app.py
@@ -0,0 +1,22 @@
+# Import Flask modules
+from flask import Flask, render_template, request
+
+# Create an object named app
+app = Flask(__name__)
+
+# Create a function named `home` which uses template file named `main.html` given under `templates` folder,
+# send your name as template variable, and assign route of no path ('/')
+
+
+# Write a function named `greet` which uses template file named `greet.html` given under `templates` folder. it takes parameters from query string on URL, assign that parameter to the 'user' variable and sent that user name into the html file. If it doesn't have any parameter, warning massage is raised
+
+
+# Write a function named `login` which uses `GET` and `POST` methods,
+# and template files named `login.html` and `secure.html` given under `templates` folder
+# and assign to the static route of ('login'). It controls If password is clarusway or not
+
+
+# Add a statement to run the Flask application which can be reached from any host on port 80.
+if __name__ == '__main__':
+    app.run(debug=True)
+# app.run(host='0.0.0.0', port=80)
