6eb875717b41077d322135d9d971cf9771b674dd
skoc <skoc10ds@gmail.com>
2021-11-28 12:18:13 +0300

    pro4 falder name changed

---
+++ aws/Project4-Blog-Page-App-Django/userdata.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+apt-get update -y
+apt-get install git -y
+apt-get install python3 -y
+cd /home/ubuntu/
+#TOKEN=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+#git clone https://$TOKEN@<YOUR GITHUB REPO URL>
+git clone https://github.com/skoc10/my_projects.git
+cd /home/ubuntu/my_projects/aws/Project4-Blog-Page-App-(Django)
+apt install python3-pip -y
+apt-get install python3.7-dev libmysqlclient-dev -y
+pip3 install -r requirements.txt
+cd /home/ubuntu/my_projects/aws/Project4-Blog-Page-App-(Django)/src
+python3 manage.py collectstatic --noinput
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py runserver 0.0.0.0:80
