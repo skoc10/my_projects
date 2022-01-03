8609b7fa7d561126300b8128d89c02875f86b3c7
skoc <skoc10ds@gmail.com>
2022-01-03 13:04:10 +0300

    terraform project

---
+++ terraform/Project1-Phonebook-Application/variables.tf
@@ -0,0 +1,42 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-east-1"
+}
+
+# Amazon Linux 2 AMI (HVM) - Kernel 5.10
+variable "aws_ami" {
+  default = {
+    "us-east-1" = "ami-0ed9277fb7eb570c9"
+
+  }
+}
+
+variable "availability_zones" {
+  default     = "us-east-1b,us-east-1c,us-east-1d,us-east-1e"
+  description = "List of availability zones, use AWS CLI to find your "
+}
+
+variable "key_name" {
+  description = "Name of AWS key pair"
+  default = "key"
+}
+
+variable "instance_type" {
+  default     = "t2.micro"
+  description = "AWS instance type"
+}
+
+variable "asg_min" {
+  description = "Min numbers of servers in ASG"
+  default     = "1"
+}
+
+variable "asg_max" {
+  description = "Max numbers of servers in ASG"
+  default     = "3"
+}
+
+variable "asg_desired" {
+  description = "Desired numbers of servers in ASG"
+  default     = "2"
+}
