735eb8546a39b39df8c9f565896c5d5dc1083a1f
skoc <skoc10ds@gmail.com>
2022-01-05 19:58:25 +0300

    autoscalling

--- terraform/Project1-Phonebook-Application/main.tf
+++ terraform/Project1-Phonebook-Application/main.tf
@@ -225,6 +225,7 @@ resource "aws_autoscaling_policy" "asgp" {
 }
 
 resource "aws_autoscaling_group" "web_asg" {
+  depends_on = [aws_launch_configuration.web_lt]
   vpc_zone_identifier  = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
   name                 = "terraform-asg"
   max_size             = var.asg_max
@@ -234,11 +235,20 @@ resource "aws_autoscaling_group" "web_asg" {
   health_check_type         = "ELB"
   force_delete         = true
   launch_configuration = aws_launch_configuration.web_lt.name
-  load_balancers       = [aws_lb.web_alb.id]
+  #load_balancers       = [aws_lb.web_alb.id]
+
+  /* lifecycle {
+    create_before_destroy = true
+  } */
 
   lifecycle {
-    create_before_destroy = true
+    ignore_changes = [load_balancers, target_group_arns]
   }
+}
+resource "aws_autoscaling_attachment" "asg_attachment_bar" {
+  autoscaling_group_name = aws_autoscaling_group.web_asg.id
+  alb_target_group_arn   = aws_lb_target_group.tg.arn
+  #elb                    = aws_lb.web_alb.id
 }
 
 data "template_file" "init" {
@@ -255,7 +265,7 @@ resource "aws_launch_configuration" "web_lt" {
   instance_type = var.instance_type
   security_groups = [aws_security_group.lt_sg.id]
 
-  user_data       = "${data.template_file.init.rendered}"
+  user_data       = data.template_file.init.rendered
  
   key_name        = var.key_name
 }
