915498f1f87e54c3ff28e78d1aeb7f258899187b
skoc <skoc10ds@gmail.com>
2022-01-08 16:39:06 +0300

    userdata phonenumber

--- terraform/Project1/user-data.sh
+++ terraform/Project1/user-data.sh
@@ -1,9 +1,20 @@
-#! /bin/bash
+#!/bin/bash
+
 yum update -y
 yum install python3 -y
 pip3 install flask
-pip3 install flask_mysql
-yum install git -y
-TOKEN="xxxxxxxxxxxxxxx"
-cd /home/ec2-user && git clone https://$TOKEN@github.com/timothy-clark/phonebook.git
-python3 /home/ec2-user/phonebook/phonebook-app.py
+pip3 install flask-mysql
+pip3 install sqlalchemy
+pip3 install Flask-SQLAlchemy
+cd /home/ec2-user
+#echo "${rds_endpoint}" > dbserver.endpoint
+#chmod 777 dbserver.endpoint
+wget https://raw.githubusercontent.com/skoc10/my_projects/main/aws/Project3-Phonebook-Application/phonebook-app.py
+mkdir templates
+cd templates
+wget https://raw.githubusercontent.com/skoc10/my_projects/main/aws/Project3-Phonebook-Application/templates/index.html
+wget https://raw.githubusercontent.com/skoc10/my_projects/main/aws/Project3-Phonebook-Application/templates/delete.html  
+wget https://raw.githubusercontent.com/skoc10/my_projects/main/aws/Project3-Phonebook-Application/templates/add-update.html
+cd ..
+python3 /home/ec2-user/phonebook-app.py
+#python3 phonebook-app.py
